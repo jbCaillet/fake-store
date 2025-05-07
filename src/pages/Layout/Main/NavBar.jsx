b1278bf8c594184b0c6ceee203837d2de2186179
jCaillet <jb.cailletbois@gmail.com>
2025-05-07 14:47:39 -0300

    fantasma

--- src/pages/Layout/Main/NavBar.jsx
+++ src/pages/Layout/Main/NavBar.jsx
@@ -1,9 +1,15 @@
-import React, { useContext } from 'react'
+import React, { useContext,useState } from 'react'
 import { Link } from 'react-router'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import { UserContext } from '../../../context/UserContext'
 
 const NavBar = () => {
+  const [showImage, setShowImage] = useState(false);
+
+  const handleAnimation = () => {
+      setShowImage(true);
+      setTimeout(() => setShowImage(false), 3000); // Oculta la imagen después de 3 segundos
+  };
   const { handleLogOut, isLoggedIn, userLoged } = useContext(UserContext);
   return (
     <div>
@@ -21,6 +27,11 @@ const NavBar = () => {
             </Nav>
           </Navbar.Collapse>
           {!isLoggedIn && <Nav.Link as={Link} to="/login">Iniciar sesión</Nav.Link>}
+          <button className="animate-button" onClick={handleAnimation}>
+                NO TOCAR
+            </button>
+            {showImage && <img src="https://http2.mlstatic.com/D_899515-MLA48452521734_122021-C.jpg" alt="Animación" className="animated-image" />}
+        
           
           {isLoggedIn && (
             <Nav.Link as="span" onClick={handleLogOut} style={{ cursor: 'pointer' }}>
