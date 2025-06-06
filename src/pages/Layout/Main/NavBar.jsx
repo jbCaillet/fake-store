5e430a909e6c846932c903dfc31882f008cd9c27
jCaillet <jb.cailletbois@gmail.com>
2025-06-06 17:22:16 -0300

    fix imagen

--- src/pages/Layout/Main/NavBar.jsx
+++ src/pages/Layout/Main/NavBar.jsx
@@ -4,12 +4,7 @@ import { Container, Nav, Navbar } from 'react-bootstrap'
 import { UserContext } from '../../../context/UserContext'
 
 const NavBar = () => {
-  const [showImage, setShowImage] = useState(false);
-
-  const handleAnimation = () => {
-      setShowImage(true);
-      setTimeout(() => setShowImage(false), 3000); // Oculta la imagen después de 3 segundos
-  };
+ 
   const { handleLogOut, isLoggedIn, userLoged } = useContext(UserContext);
   return (
     <div>
@@ -22,16 +17,12 @@ const NavBar = () => {
               <Nav.Link as={Link} to="/">Inicio</Nav.Link>
               <Nav.Link as={Link} to="/productos">Productos</Nav.Link>
               <Nav.Link as={Link} to="/cualquier-url">404</Nav.Link>
-              <Nav.Link as={Link} to="/user">Mi Cuenta</Nav.Link>
+              {isLoggedIn &&  <Nav.Link as={Link} to="/user">Mi Cuenta</Nav.Link>}
               {userLoged.role == "admin" && <Nav.Link as={Link} to="/agregar-producto">Agregar producto</Nav.Link>}
             </Nav>
           </Navbar.Collapse>
           {!isLoggedIn && <Nav.Link as={Link} to="/login">Iniciar sesión</Nav.Link>}
-          <button className="animate-button" onClick={handleAnimation}>
-                NO TOCAR
-            </button>
-            {showImage && <img src="https://http2.mlstatic.com/D_899515-MLA48452521734_122021-C.jpg" alt="Animación" className="animated-image" />}
-        
+
           
           {isLoggedIn && (
             <Nav.Link as="span" onClick={handleLogOut} style={{ cursor: 'pointer' }}>
